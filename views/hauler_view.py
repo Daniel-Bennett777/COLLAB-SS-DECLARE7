39da481e922b91c708cf39f92109bd7d0d3f1235
Michelle Totherow <tym4me13@gmail.com>
2023-10-20 02:05:46 -0500

    add post

--- views/hauler_view.py
+++ views/hauler_view.py
@@ -1,6 +1,6 @@
 import json
 from nss_handler import status
-from repository import db_get_single, db_get_all, db_delete, db_update
+from repository import db_get_single, db_get_all, db_delete, db_update, db_create
 
 class HaulerView():
 
@@ -45,3 +45,18 @@ class HaulerView():
             return handler.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
         else:
             return handler.response("", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+
+    def insert(self, handler, hauler_data):
+        sql = """
+        INSERT INTO Hauler 
+        VALUES(null, ?,?)
+                
+        """
+                
+        new_item = db_create(sql,(hauler_data['name'], hauler_data['dock_id']))
+        
+        if new_item > 0:
+            return handler.response("", status.HTTP_201_SUCCESS_CREATED.value)
+        else:
+            return handler.response("", status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value)
+        
